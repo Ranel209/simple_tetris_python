2627c2dbc26d18e5caab5e9c08ce4052d1929511
Adrian Ranel <adrianranelm@gmail.com>
2025-01-29 00:06:07 +0800

    txt highscore screen added

--- tetris_python.py
+++ tetris_python.py
@@ -2,6 +2,7 @@ import pygame
 from copy import deepcopy
 from random import choice, randrange
 
+<<<<<<< HEAD
 # Creating the game window
 width, height = 10, 15  
 tile = 45  
@@ -229,3 +230,15 @@ while True:
 
     pygame.display.flip()
     clock.tick(FPS)
+=======
+width, height = 10, 15  # Width and Height of the game grid
+tile = 45  # Size of a tile
+game_res = width * tile, height * tile  # Game surface resolution
+res = 750, 940  # Window resolution
+FPS = 60
+
+pygame.init()
+sc = pygame.display.set_mode(res)  # Main screen
+game_sc = pygame.Surface(game_res)  # Game surface (grid area)
+clock = pygame.time.Clock()
+>>>>>>> 22a2f6e (game window)
